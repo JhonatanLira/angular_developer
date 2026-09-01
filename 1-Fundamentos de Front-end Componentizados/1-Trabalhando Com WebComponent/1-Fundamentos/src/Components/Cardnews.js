475748ceac62582c02e73489f26d3a45249fc752
Jhonatan <jhonatanlira@gmail.com>
2026-09-01 16:02:21 -0300

    setando propriedades de classes

--- 1-Fundamentos de Front-end Componentizados/1-Trabalhando Com WebComponent/1-Fundamentos/src/Components/Cardnews.js
+++ 1-Fundamentos de Front-end Componentizados/1-Trabalhando Com WebComponent/1-Fundamentos/src/Components/Cardnews.js
@@ -1,10 +1,33 @@
-class CardNews extends HTMLElement {
+class Cardnews extends HtmlElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello world</h1>";
+        const shadow = this.attachShadow({ mode: "open" });
+
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class","card");   
+
+        const cardLeft = document.createElement("div");
+        //cardLeft.setAttribute("class","card_left");
+
+        const cardRight = document.createElement("div");
+        //cardRight.setAttribute("class","card_right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+
+    styles() {
+
     }
 }
 
-customElements.define('card-news', CardNews);
+customElements.define("card-news", Cardnews);
